4ced5e672440c33bf1cc40b3fd011e7422b05bd8
sktech730 <sktech730@gmail.com>
2021-02-11 14:33:48 -0500

    Update handler.py

--- src/handler.py
+++ src/handler.py
@@ -1,8 +1,10 @@
 import json
 import datetime
+import boto3
 
 
 def endpoint(event, context):
+    sync_ddb_table(event['source_db'],event['dest_db'])
     current_time = datetime.datetime.now().time()
     body = {
         "message": "Hello, the current time is " + str(current_time)
@@ -14,3 +16,8 @@ def endpoint(event, context):
     }
 
     return response
+
+def sync_ddb_table(source_ddb, destination_ddb):
+    response = source_ddb.scan(TableName=“table1”)
+    for item in response['Items']:
+        destination_ddb.put_item(TableName=“table2”, Item=item)
