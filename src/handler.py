5d8be0afe4dd417db3b42dcca2ee4967cbdc2e99
kulsant <kulsant@amazon.com>
2021-02-12 14:42:23 -0500

    Test codeguru reviewer -1

--- src/handler.py
+++ src/handler.py
@@ -4,9 +4,11 @@ import boto3
 
 
 def endpoint(event, context):
-    source_ddb = boto3.client('dynamodb','us-east-1')
-    destination_ddb = boto3.client('dynamodb','us-west-1')
-    sync_ddb_table(event['source_db'], event['dest_db'])
+    filepath = 'demo.tx'
+    read_lines(filepath)
+    source_ddb = boto3.client('dynamodb', 'us-east-1')
+    destination_ddb = boto3.client('dynamodb', 'us-west-1')
+    sync_ddb_table(source_ddb, destination_ddb)
     current_time = datetime.datetime.now().time()
     body = {
         "message": "Hello, the current time is " + str(current_time)
@@ -19,7 +21,17 @@ def endpoint(event, context):
     return response
 
 
-def sync_ddb_table(source_ddb, destination_ddb):
-    response = source_ddb.scan(TableName='table1')
+# Code Showing AWS pagination Issue
+def sync_ddb_table(srs_ddb, dest_ddb):
+    response = srs_ddb.scan(TableName='table1')
     for item in response['Items']:
-        destination_ddb.put_item(TableName='table2', Item=item)
+        dest_ddb.put_item(TableName='table2', Item=item)
+
+
+# Resource leak Code
+def read_lines(file):
+    lines = []
+    f = open(file, 'r')
+    for line in f:
+        lines.append(line.strip('\n').strip('\r\n'))
+    return lines
