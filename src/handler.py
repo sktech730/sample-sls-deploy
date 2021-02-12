7a70d17300055da21c66153cdd82aaaf2b4b885b
kulsant <kulsant@amazon.com>
2021-02-12 10:14:12 -0500

    Test codeguru reviewer

--- src/handler.py
+++ src/handler.py
@@ -4,12 +4,13 @@ import boto3
 
 
 def endpoint(event, context):
-    sync_ddb_table(event['source_db'],event['dest_db'])
+    source_ddb = boto3.client('dynamodb','us-east-1')
+    destination_ddb = boto3.client('dynamodb','us-west-1')
+    sync_ddb_table(event['source_db'], event['dest_db'])
     current_time = datetime.datetime.now().time()
     body = {
         "message": "Hello, the current time is " + str(current_time)
     }
-
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
@@ -17,7 +18,8 @@ def endpoint(event, context):
 
     return response
 
+
 def sync_ddb_table(source_ddb, destination_ddb):
-    response = source_ddb.scan(TableName=“table1”)
+    response = source_ddb.scan(TableName='table1')
     for item in response['Items']:
-        destination_ddb.put_item(TableName=“table2”, Item=item)
+        destination_ddb.put_item(TableName='table2', Item=item)
